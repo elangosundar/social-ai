e6fa761d496d845e3ec3bee6efc3c8931cca5d56
elangosundar <elangovan2me@gmail.com>
2020-09-02 17:42:42 +0530

    added the all social links

--- utils/util.js
+++ utils/util.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const socialLinks = require('./social');
+const social = require('./social');
 
 const _getSocialLinks = (socialData) => {
 
 	const getSocialLink = ( socialName, username ) => {
+		console.log(social.socialLinks);
 		const imgLink = `https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/${socialName}.svg`;
 		const socLink = socialLinks[socialName] ? socialLinks[socialName].userName : '';
-console.log(socLink);
+// console.log(socLink);
 		return `<a href="${socLink}" target="_blank"><img align="center" src="${imgLink}" alt="${username}" height="20" width="20" /></a>`;
 	};
 
